3b0b1cc6d846ad4b11ec1eeb334103aee6158baa
robyngoodner <robyn.goodner@gmail.com>
2022-04-23 16:43:37 -0400

    Removes QR scanner attempt

--- main_app/static/scripts/app.js
+++ main_app/static/scripts/app.js
@@ -36,13 +36,3 @@ function hide(id) {
 
 
 
-
-$('#reader').html5_qrcode(function(data){
-    // do something when code is read
-},
-function(error){
-  //show read errors 
-}, function(videoError){
-  //the video stream could be opened
-}
-);
