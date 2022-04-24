d2d77a714d56c815dd92c229426b724973527d1f
robyngoodner <robyn.goodner@gmail.com>
2022-04-23 22:16:25 -0400

    Styles front page and logged in front page

--- sQuaRe_change/settings.py
+++ sQuaRe_change/settings.py
@@ -20,7 +20,7 @@ if not os.environ.get('PRODUCTION'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = BASE_DIR / 'static' # add static files
+# STATIC_ROOT = BASE_DIR / 'static' # add static files
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -29,7 +29,7 @@ STATIC_ROOT = BASE_DIR / 'static' # add static files
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
