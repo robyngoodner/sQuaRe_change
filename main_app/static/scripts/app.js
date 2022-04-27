7e016a2b1e482579317fed0bfca3214c0756fce1
robyngoodner <robyn.goodner@gmail.com>
2022-04-27 00:11:26 -0400

    Adds login to show login and registration back in

--- main_app/static/scripts/app.js
+++ main_app/static/scripts/app.js
@@ -2,26 +2,26 @@ console.log("my script is connected")
 
 
 
-// document.getElementById('toggle_login').addEventListener('click', 
-// function(e){
-//     e.preventDefault();
-//     console.log('clickedd')
-//     document.getElementById('hidden').style.display = 'flex';
-//     document.getElementById('login').style.display = "none";
-//     document.getElementById('register').style.display = 'flex';
-//     document.getElementById('hidden2').style.display="none";
-//     })
+document.getElementById('toggle_login').addEventListener('click', 
+function(e){
+    e.preventDefault();
+    console.log('clickedd')
+    document.getElementById('hidden').style.display = 'flex';
+    document.getElementById('login').style.display = "none";
+    document.getElementById('register').style.display = 'flex';
+    document.getElementById('hidden2').style.display="none";
+    })
 
-// document.getElementById('toggle_register').addEventListener('click', 
-// function(e){
-//     e.preventDefault();
-//     console.log('clickedd!')
-//     document.getElementById('hidden2').style.display = 'flex';
-//     document.getElementById('login').style.display = "flex";
-//     document.getElementById('register').style.display = 'none';
-//     document.getElementById('hidden').style.display = "none";
+document.getElementById('toggle_register').addEventListener('click', 
+function(e){
+    e.preventDefault();
+    console.log('clickedd!')
+    document.getElementById('hidden2').style.display = 'flex';
+    document.getElementById('login').style.display = "flex";
+    document.getElementById('register').style.display = 'none';
+    document.getElementById('hidden').style.display = "none";
 
-//     })
+    })
 
 
 let printButton = document.getElementById('print')
